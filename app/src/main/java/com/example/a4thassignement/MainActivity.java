861a8fe1b293396cb09c49d5f98b039ca69c2146
teroikaheimo <tero.ikaheimo@gmail.com>
2019-09-27 11:23:23 +0300

    Added button and editText to mainActivity and added button funtionality.

--- app/src/main/java/com/example/a4thassignement/MainActivity.java
+++ app/src/main/java/com/example/a4thassignement/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.a4thassignement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonToggle;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.buttonToggle = findViewById(R.id.buttonToggle);
+        this.editText = findViewById(R.id.editText);
+
+        buttonToggle.setOnClickListener(new View.OnClickListener(
+        ) {
+            @Override
+            public void onClick(View view) {
+                toggleEditTextField();
+            }
+        });
+    }
+
+    // Disables or enables editText field.
+    private void toggleEditTextField() {
+        boolean setState = !editText.isFocusable();
+        this.editText.setFocusable(setState);
+        this.editText.setFocusableInTouchMode(setState);
+        this.editText.setEnabled(setState);
     }
 }
