f88e3ee25ff8fee58e561819c15473c95479f53d
teroikaheimo <tero.ikaheimo@gmail.com>
2019-10-02 16:52:19 +0300

    Basic state preserving works.

--- app/src/main/java/com/example/a4thassignement/MainActivity.java
+++ app/src/main/java/com/example/a4thassignement/MainActivity.java
@@ -7,6 +7,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
+/*
+* Create an application which shows a splash screen when starting.
+* The screen shows the name of the application and the logo of the application in the middle
+* of the screen. The splash screen lasts for 3,5 seconds before moving on.
+
+The next screen, which is the main screen of the application, consists of a EditText and a Button.
+* The button enables/disables the EditText.
+
+When the phone orientation changes from vertical to horizontal (or horizontal to vertical)
+* the text inputted in the EditText stays the same. Also the EditText stays enabled or disabled
+* after the orientation change.
+
+After this implement a general solution of your choice to save the state of the views in your
+* activity. Meaning that you are not hardcoding the views when saving the states but instead
+* somehow iterating through all necessary views used for state.
+* */
+
 public class MainActivity extends AppCompatActivity {
     Button buttonToggle;
     EditText editText;
@@ -15,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get views
         this.buttonToggle = findViewById(R.id.buttonToggle);
         this.editText = findViewById(R.id.editText);
 
+        // toggle editText on and off
         buttonToggle.setOnClickListener(new View.OnClickListener(
         ) {
             @Override
@@ -27,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        // Saves editText State
         if (!this.editText.isFocusable()) {
             outState.putBoolean("editTextDisabled", true);
         }
@@ -38,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedState) {
         super.onRestoreInstanceState(savedState);
+        // restores editText State
         if (savedState.getBoolean("editTextDisabled")) {
             toggleEditTextField(false);
         }
